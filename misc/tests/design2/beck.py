7810eebfaf41d32f227d18d9550960316022626a
James Rowland <James.Rowland@diamond.ac.uk>
2011-04-08 15:24:15 +0000

    ethercat: combining config files into one

--- misc/tests/design2/beck.py
+++ misc/tests/design2/beck.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/python2.4
+
+# need system python for libxml2 support with xpath
 
 import libxml2
 
-reqs = set([(0x00000002, 0x044d2c52, 0x00110000),
-            (0x00000002, 0x03ec3052, 0x00100000),
-            (0x00000002, 0x05de3052, 0x00100000)])
+## reqs = set([(0x00000002, 0x044d2c52, 0x00110000),
+##             (0x00000002, 0x03ec3052, 0x00100000),
+##             (0x00000002, 0x05de3052, 0x00100000)])
+
+reqs = set()
 
 def parsePdoEntry(entry):
     # some pdo entries are just padding with no name, ignore
@@ -32,7 +36,7 @@ def parsePdo(pdo, d):
     print '    </sync>'
     
 def parseInt(text):
-    if text.startswith("#x"):
+    if text.startswith("#x") or text.startswith("0x"):
         return int(text.replace("#x", ""), 16)
     else:
         return int(text)
@@ -42,13 +46,14 @@ def parseFile(filename):
     vendor = parseInt(doc.xpathEval("//Vendor/Id")[0].content)
     for device in doc.xpathEval("//Device"):
         try:
+            name = device.xpathEval("Type")[0].content
             product = parseInt(device.xpathEval("Type/@ProductCode")[0].content)
+            revision = parseInt(device.xpathEval("Type/@RevisionNo")[0].content)
         except:
             continue
-        revision = parseInt(device.xpathEval("Type/@RevisionNo")[0].content)
-        key = (vendor, product, revision)
+        # key = (vendor, product, revision)
+        key = (name, revision)
         if key in reqs:
-            name = device.xpathEval("Type")[0].content
             print '  <device name="%s" vendor="0x%08x" product="0x%08x" revision="0x%08x">' % (name, vendor, product, revision)
             for txpdo in device.xpathEval("TxPdo"):
                 parsePdo(txpdo, 1)
@@ -57,13 +62,28 @@ def parseFile(filename):
             print '  </device>'
 
 if __name__ == "__main__":
-    # filename = "/home/jr76/ethercat/xml/Beckhoff EKxxxx.xml"
+
+    chain = "chain.xml"
+    doc = libxml2.parseFile(chain)
+
+    for d in doc.xpathEval("//device"):
+        name = d.xpathEval("@type_name")[0].content
+        revision = parseInt(d.xpathEval("@revision")[0].content)
+        reqs.add((name, revision))
+
     base = "/home/jr76/ethercat/xml"
     import os
+    # print "<scanner>"
     print "<devices>"
     for f in os.listdir(base):
         if f.endswith("xml"):
             filename = os.path.join(base, f)
             # print filename
             parseFile(filename)
+
     print "</devices>"
+    # print doc.xpathEval("/chain")[0]
+    # print "</scanner>"
+
+# loads chain description, outputs complete config file...
+
