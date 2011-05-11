4c9df88ae467c9cdaa9c9d3be81579a63da7235d
James Rowland <James.Rowland@diamond.ac.uk>
2011-05-11 09:19:35 +0000

    ethercat: move templates to support module

--- iocs/scanTest/st.cmd
+++ iocs/scanTest/st.cmd
@@ -1,11 +1,11 @@
-#!../../bin/linux-x86/ethercat
-dbLoadDatabase("../../dbd/ecAsyn.dbd")
-ethercat_registerRecordDeviceDriver(pdbbase)
+#!bin/linux-x86/scantest
+dbLoadDatabase("dbd/scantest.dbd")
+scantest_registerRecordDeviceDriver(pdbbase)
 ecAsynInit("/tmp/socket", 1000000)
 
-dbLoadRecords("ecApp/Db/MASTER.template", "DEVICE=JRM,PORT=MASTER0,SCAN=I/O Intr")
-dbLoadRecords("ecApp/Db/EK1101.template", "DEVICE=JRC,PORT=COUPLER0,SCAN=I/O Intr")
-dbLoadRecords("ecApp/Db/EL1004.template", "DEVICE=JRV,PORT=VACUUM0,SCAN=I/O Intr")
-dbLoadRecords("ecApp/Db/EL3702.template", "DEVICE=JRR,PORT=RF0,SCAN=I/O Intr")
-dbLoadRecords("ecApp/Db/EL2004.template", "DEVICE=JRO,PORT=OUT0,SCAN=I/O Intr")
+dbLoadRecords("scantestApp/Db/MASTER.template", "DEVICE=JRM,PORT=MASTER0,SCAN=I/O Intr")
+dbLoadRecords("scantestApp/Db/EK1101.template", "DEVICE=JRC,PORT=COUPLER0,SCAN=I/O Intr")
+dbLoadRecords("scantestApp/Db/EL1004.template", "DEVICE=JRV,PORT=VACUUM0,SCAN=I/O Intr")
+dbLoadRecords("scantestApp/Db/EL3702.template", "DEVICE=JRR,PORT=RF0,SCAN=I/O Intr")
+dbLoadRecords("scantestApp/Db/EL2004.template", "DEVICE=JRO,PORT=OUT0,SCAN=I/O Intr")
 iocInit()
