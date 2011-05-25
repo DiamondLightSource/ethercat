38030fd38d61d371303a1680d66a7e1967e5ff59
James Rowland <James.Rowland@diamond.ac.uk>
2011-05-25 14:54:38 +0000

    ethercat: tested 1m sample NI acquisition

--- iocs/scanTest/stni.cmd
+++ iocs/scanTest/stni.cmd
@@ -1,9 +1,12 @@
 #!bin/linux-x86/scantest
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","10000000")
+
 dbLoadDatabase("dbd/scantest.dbd")
 scantest_registerRecordDeviceDriver(pdbbase)
 ecAsynInit("/tmp/socket", 1000000)
 
 dbLoadRecords("../../db/NI944.template", "DEVICE=JRTESTNI,PORT=NI0,SCAN=.1 second")
-dbLoadRecords("../../db/gadc.template", "DEVICE=JRTESTNI,PORT=NI0,SCAN=.1 second,CHANNEL=4")
+dbLoadRecords("../../db/gadc.template", "DEVICE=JRTESTNI,PORT=NI0,SCAN=.1 second,CHANNEL=4,SAMPLES=1000000")
 
 iocInit()
