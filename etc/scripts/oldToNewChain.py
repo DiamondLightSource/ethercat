f9b95a91afddf4cb0dd9c3ffa9054d85764410a4
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2013-03-19 15:18:08 +0000

    ethercat: added conversion script from old to new chain

---
+++ etc/scripts/oldToNewChain.py
@@ -0,0 +1,25 @@
+#!/bin/env dls-python2.6
+
+from xml.dom.minidom import parse
+import sys
+
+doc = parse(sys.argv[1])
+chain = doc.getElementsByTagName("chain")[0]
+chain.tagName = "components"
+chain.setAttribute("arch", "linux-x86")
+m = doc.createElement("ethercat.EthercatMaster")
+m.setAttribute("name", "ECATM")
+m.setAttribute("socket","/tmp/socket")
+chain.insertBefore(m, chain.firstChild)
+for device in chain.getElementsByTagName("device"):
+    type_name = device.getAttribute("type_name")
+    device.removeAttribute("type_name")
+    revision = device.getAttribute("revision")
+    device.removeAttribute("revision")
+    device.setAttribute("type_rev", "%s rev %s" % (type_name, revision))
+    device.tagName = "ethercat.EthercatSlave"
+    device.setAttribute("master", "ECATM")
+print doc.toxml()
+
+
+
